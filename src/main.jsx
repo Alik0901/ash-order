d3b99c4d343ee6c7488e8d0be4ee4bdf0ad4d7df
AshBuilder <ash@prah.dev>
2025-04-09 11:00:19 +0300

    chore: add WagmiProvider with Scroll network config

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App.jsx';
+import './index.css';
+import './App.css';
+
+import {
+  WagmiProvider,
+  createConfig,
+  configureChains,
+} from 'wagmi';
+import { scrollSepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [scrollSepolia],
+  [publicProvider()],
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
+    <WagmiProvider config={config}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </WagmiProvider>
   </React.StrictMode>
 );
