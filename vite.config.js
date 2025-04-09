727319c4b9365f82370218da2fd046adaf1629af
AshBuilder <ash@prah.dev>
2025-04-09 17:10:34 +0300

    fix: final changes for TON integration and Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@tonconnect/sdk'],  // Явно указываем, чтобы Vite правильно обработал
-  },
   build: {
-    rollupOptions: {
-      external: ['@tonconnect/sdk'], // Убираем ошибку при сборке на Vercel
+    commonjsOptions: {
+      include: [/node_modules/],
     },
   },
 });
