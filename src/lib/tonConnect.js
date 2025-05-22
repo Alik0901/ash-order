a62ddbc65336919c136a4c614f2ecbe538914b73
AshBuilder <ash@prah.dev>
2025-05-22 20:50:13 +0300

    fix: set correct walletsListSource

--- src/lib/tonConnect.js
+++ src/lib/tonConnect.js
@@ -3,14 +3,7 @@ import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 
-let instance;
-
-export function getTonConnectInstance() {
-  if (!instance) {
-    instance = new TonConnect({
-      manifestUrl: 'https://ash-order.vercel.app/tonconnect-manifest.json',
-      walletsListSource: 'https://ton-connect.github.io/wallets-list/wallets.json' // ✅ Указать напрямую
-    });
-  }
-  return instance;
-}
+export const tonConnect = new TonConnect({
+  manifestUrl: 'https://ash-order.vercel.app/tonconnect-manifest.json',
+  walletsListSource: 'https://raw.githubusercontent.com/ton-connect/wallets-list/main/wallets.json'
+});
