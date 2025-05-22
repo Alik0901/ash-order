958a7b47e5d06278e713832e68405d0408f5c2f6
AshBuilder <ash@prah.dev>
2025-05-22 19:30:18 +0300

    fix: force TonConnect to use TelegramProvider explicitly

--- src/lib/tonConnect.js
+++ src/lib/tonConnect.js
@@ -1,10 +1,13 @@
 // src/lib/tonConnect.js
 import { TonConnect } from '@tonconnect/sdk';
+import { TelegramProvider } from '@tonconnect/sdk/providers/telegram';
 import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 
 export const tonConnect = new TonConnect({
   manifestUrl: 'https://ash-order.vercel.app/tonconnect-manifest.json',
-  walletsListSource: 'telegram'
+  walletConnectParameters: {
+    provider: new TelegramProvider() // Принудительно задаём Telegram-провайдер
+  }
 });
