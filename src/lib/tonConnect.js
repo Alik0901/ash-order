20b8e973d388aee7fd3b785abd3a18bce3a0c803
AshBuilder <ash@prah.dev>
2025-05-22 18:34:04 +0300

    fix: correct manifestUrl for TON Connect

--- src/lib/tonConnect.js
+++ src/lib/tonConnect.js
@@ -6,5 +6,5 @@ WebApp.ready();
 
 export const tonConnect = new TonConnect({
   manifestUrl: 'https://ash-order.vercel.app/tonconnect-manifest.json',
-  walletsListSource: 'telegram' // ВАЖНО: иначе SDK не определяет окружение
+  walletsListSource: 'telegram'
 });
