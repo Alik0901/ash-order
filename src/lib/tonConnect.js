c36eb8a24ef4abd5f2fc0989fd1dc93c008da7e8
AshBuilder <ash@prah.dev>
2025-05-22 19:34:20 +0300

    fix: remove invalid TelegramProvider import, use walletsListSource

--- src/lib/tonConnect.js
+++ src/lib/tonConnect.js
@@ -1,13 +1,10 @@
 // src/lib/tonConnect.js
 import { TonConnect } from '@tonconnect/sdk';
-import { TelegramProvider } from '@tonconnect/sdk/providers/telegram';
 import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 
 export const tonConnect = new TonConnect({
   manifestUrl: 'https://ash-order.vercel.app/tonconnect-manifest.json',
-  walletConnectParameters: {
-    provider: new TelegramProvider() // Принудительно задаём Telegram-провайдер
-  }
+  walletsListSource: 'telegram'
 });
